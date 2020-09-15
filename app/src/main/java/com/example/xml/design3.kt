682c31199491c7344a04938c619d32a2a2ef988e
omar“git config --global user.email engalshikhomar@gmail.com“ <engomaralshikh@gmail.com>
2020-09-15 23:01:29 +0300

    الواجهه الاخيرة

---
+++ app/src/main/java/com/example/xml/design3.kt
@@ -0,0 +1,12 @@
+package com.example.xml
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class design3 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_design3)
+    }
+}
