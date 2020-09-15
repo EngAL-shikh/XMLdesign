ee1b3c6e045b16b06b603645d41b6fd6cdd6c3c3
omar“git config --global user.email engalshikhomar@gmail.com“ <engomaralshikh@gmail.com>
2020-09-15 22:46:26 +0300

    Initial commit

---
+++ app/src/main/java/com/example/xml/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.xml
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
---
+++ app/src/main/java/com/example/xml/Quran.kt
@@ -0,0 +1,12 @@
+package com.example.xml
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Quran : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quran)
+    }
+}
